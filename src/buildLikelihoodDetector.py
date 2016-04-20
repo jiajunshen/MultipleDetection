7075381ee9d5f4dd0464ebe95da1d12b4061c01d
Jiajun <shenjiajun90@gmail.com>
2016-04-20 11:27:30 -0500

    clean up project structure

---
+++ src/buildLikelihoodDetector.py
@@ -0,0 +1,12 @@
+import os
+import numpy as np
+import sys
+import time
+import theano
+import theano.tensor as T
+import lasagne
+from lasagne.layers.dnn import Conv2DDNNLayer as Conv2DLayer
+from lasagne.layers.dnn import MaxPool2DDNNLayer as MaxPool2DLayer
+
+
+
