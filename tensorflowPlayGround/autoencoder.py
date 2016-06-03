f89e27e3e9090e21196c8eafa0fe6e1d1df35fbd
Jiajun <shenjiajun90@gmail.com>
2016-06-03 00:23:01 -0500

    mlp autoencoder works

--- tensorflowPlayGround/autoencoder.py
+++ tensorflowPlayGround/autoencoder.py
@@ -1,6 +1,6 @@
 from __future__ import division, print_function
 import time
-import os.path import join as pjoin
+from os.path import join as pjoin
 import tensorflow as tf
 import numpy as np
 import math
@@ -18,8 +18,8 @@ def loss_x_entropy(output, target):
     Scalar tensor of cross entropy
   """
   with tf.name_scope("xentropy_loss"):
-      net_output_tf = tf.ops.convert_to_tensor(output, name='input')
-      target_tf = tf.ops.convert_to_tensor(target, name='target')
+      net_output_tf = tf.convert_to_tensor(output, name='input')
+      target_tf = tf.convert_to_tensor(target, name='target')
       cross_entropy = tf.add(tf.mul(tf.log(net_output_tf, name='log_output'),
                                     target_tf),
                              tf.mul(tf.log(1 - net_output_tf),
@@ -67,7 +67,7 @@ class AutoEncoder(object):
     #http://cmgreen.io/2016/01/04/tensorflow_deep_autoencoder.html
     def _setup_variables(self):
         with tf.name_scope("autoencoder_variables"):
-            for i in range(self._num_hidden_layers + 1):
+            for i in range(self._num_hidden_layers):
                 name_w = self._weights_str.format(i + 1)
                 w_shape = (self._shape[i], self._shape[i + 1])
                 # We use xavier initializer here
@@ -76,33 +76,26 @@ class AutoEncoder(object):
                 self[name_w] = tf.Variable(w_init, name = name_w, trainable = True)
 
                 name_b = self._bias_str.format(i + 1)
-                b_shape = (self._shape[i], )
+                b_shape = (self._shape[i + 1], )
                 b_init = tf.zeros(b_shape)
                 self[name_b] = tf.Variable(b_init, name = name_b, trainable = True)
+                print(w_shape, b_shape)
+            
+            #Output Layer: No weights on the output layer, we only have the bias
+
+            name_w = self._weights_str.format(self._num_hidden_layers + 1) + "_out"
+            w_shape = (self._shape[self._num_hidden_layers], self._shape[self._num_hidden_layers + 1])
+            w_init = tf.random_uniform(w_shape, -1 * initializer_bound, 1 * initializer_bound)
+            self[name_w] = tf.Variable(w_init, name = name_w, trainable = True)
+            
+            name_b = self._bias_str.format(self._num_hidden_layers + 1) + "_out"
+            b_shape = (self._shape[self._num_hidden_layers + 1], )
+            b_init = tf.zeros(b_shape)
+            self[name_b] = tf.Variable(b_init, name = name_b, trainable = True)
+            print(w_shape, b_shape)
+            print(self._variables.keys())
         
                 
-                ## Why do we need the following? we will see
-
-                if i < self.__num_hidden_layers:
-                  # Hidden layer fixed weights (after pretraining before fine tuning)
-                  self[name_w + "_fixed"] = tf.Variable(tf.identity(self[name_w]),
-                                                        name=name_w + "_fixed",
-                                                        trainable=False)
-
-                  # Hidden layer fixed biases
-                  self[name_b + "_fixed"] = tf.Variable(tf.identity(self[name_b]),
-                                                        name=name_b + "_fixed",
-                                                        trainable=False)
-
-                  # Pretraining output training biases
-                  name_b_out = self._bias_str.format(i + 1) + "_out"
-
-                  b_shape = (self.__shape[i],)
-                  b_init = tf.zeros(b_shape)
-                  self[name_b_out] = tf.Variable(b_init,
-                                                 trainable=True,
-                                                 name=name_b_out)
-
 
     def _w(self, n, suffix = ""):
         return self[self._weights_str.format(n)+suffix]
@@ -110,21 +103,9 @@ class AutoEncoder(object):
     def _b(self, n, suffix = ""):
         return self[self._bias_str.format(n) + suffix]
 
-    def get_variable_to_init(self,n):
-        assert n > 0
-        assert n <= self._num_hidden_layers + 1
-
-        vars_to_init = [self._w(n), self._b(n)]
-
-        if n <= self._num_hidden_layers:
-            vars_to_init.append(self._b(n, "_out"))
-
-        if 1 < n <= self._num_hidden_layers:
-            vars_to_init.append(self._w(n -1, "_fixed"))
-            vars_to_init.append(self._b(n - 1, "_fixed"))
 
 
-    def _activate(x, w, b, transpose_w = False):
+    def _activate(self, x, w, b, transpose_w = False):
         y = tf.sigmoid(tf.nn.bias_add(tf.matmul(x, w, transpose_b = transpose_w), b))
         return y
 
@@ -137,12 +118,12 @@ class AutoEncoder(object):
         """        
         last_output = input_pl
         for i in xrange(n - 1):
-            w = self._w(i + 1, "_fixed")
-            b = self._b(i + 1, "_fixed")
+            w = self._w(i + 1)
+            b = self._b(i + 1)
             last_output = self._activate(last_output, w, b)
         
 
-        out = self._activate(last_output, self._w(n), self._b(n, "_out"))
+        out = self._activate(last_output, self._w(n, "_out"), self._b(n, "_out"))
 
         out = tf.maximum(out, 1.e-9)
         out = tf.minimum(out, 1 - 1.e-9)
@@ -151,97 +132,121 @@ class AutoEncoder(object):
     
 
 
-    def main_unsupervised():
-      with tf.Graph().as_default() as g:
-        sess = tf.Session()
+def main_unsupervised():
+  with tf.Graph().as_default() as g:
+    sess = tf.Session()
 
-        num_hidden = FLAGS.num_hidden_layers
+    num_hidden = FLAGS.num_hidden_layers
 
-        # The shape of the hidden layers
-        ae_hidden_shapes = [getattr(FLAGS, "hidden{0}_units".format(j + 1))
-                            for j in xrange(num_hidden)]
+    # The shape of the hidden layers
+    ae_hidden_shapes = [getattr(FLAGS, "hidden{0}_units".format(j + 1))
+                        for j in xrange(num_hidden)]
 
-        # The shape of the all the layers, including the input layer and the classification layer... Why do we need the classification layer again?
-        ae_shape = [FLAGS.image_pixels] + ae_hidden_shapes + [FLAGS.num_classes]
+    # The shape of the all the layers, including the input layer and the classification layer... Why do we need the classification layer again?
+    ae_shape = [FLAGS.image_pixels] + ae_hidden_shapes + [FLAGS.image_pixels]
 
-        ae = AutoEncoder(ae_shape, sess)
-
-
-        from tensorflow.examples.tutorials.mnist import input_data
-        mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-        num_train = data.train.num_examples
-
-        learning_rates = {j: getattr(FLAGS,
-                                     "pre_layer{0}_learning_rate".format(j + 1))
-                          for j in xrange(num_hidden)}
-
-        noise = {j: getattr(FLAGS, "noise_{0}".format(j + 1))
-                 for j in xrange(num_hidden)}
-
-        with tf.variable_scope("net_training".format(n)):
-
-            input_ = tf.placeholder(dtype=tf.float32,
-                                    shape=(FLAGS.batch_size, ae_shape[0]),
-                                    name='ae_input_pl')
-            
-            target_for_loss = ae.net(input_, n)
+    ae = AutoEncoder(ae_shape, sess)
 
 
-            #loss = loss_x_entropy(layer, target_for_loss)
-            loss = tf.sqrt(tf.reduce_mean(tf.square(input_ - target_for_loss)))
+    from tensorflow.examples.tutorials.mnist import input_data
+    mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+    num_train = mnist.train.num_examples
 
 
+    n = 4
+    with tf.variable_scope("net_training"):
+
+        input_ = tf.placeholder(dtype=tf.float32,
+                                shape=(FLAGS.batch_size, ae_shape[0]),
+                                name='ae_input_pl')
         
+        target_for_loss = ae.net(input_, n)
 
 
-            summary_dir = pjoin(FLAGS.summary_dir, 'pretraining_{0}'.format(n))
-            summary_writer = tf.train.SummaryWriter(summary_dir,
-                                                    graph_def=sess.graph_def,
-                                                    flush_secs=FLAGS.flush_secs)
-            summary_vars = [ae["biases{0}".format(n)], ae["weights{0}".format(n)]]
+        loss = loss_x_entropy(target_for_loss, input_)
+        #loss = tf.sqrt(tf.reduce_mean(tf.square(input_ - target_for_loss)))
 
-            hist_summarries = [tf.histogram_summary(v.op.name, v)
-                               for v in summary_vars]
-            
-            summary_op = tf.merge_summary(hist_summarries)
 
-            vars_to_init = ae.get_variables_to_init(n)
-            vars_to_init.append(global_step)
-            sess.run(tf.initialize_variables(vars_to_init))
+    
 
-            train_step = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
 
-            print("\n\n")
-            print("| Training Step | Cross Entropy |  Layer  |   Epoch  |")
-            print("|---------------|---------------|---------|----------|")
+        summary_dir = pjoin(FLAGS.summary_dir, 'auto_encoder_training')
+        summary_writer = tf.train.SummaryWriter(summary_dir,
+                                                graph_def=sess.graph_def,
+                                                flush_secs=FLAGS.flush_secs)
+        summary_vars = [ae[key] for key in ae._variables.keys()]
 
-            for step in xrange(FLAGS.pretraining_epochs * num_train):
-              feed_dict = fill_feed_dict_ae(data.train, input_, target_, noise[i])
+        hist_summarries = [tf.histogram_summary(v.op.name, v)
+                           for v in summary_vars]
+        
+        
+        summary_op = tf.merge_summary(hist_summarries)
 
-              loss_summary, loss_value = sess.run([train_op, loss],
-                                                  feed_dict=feed_dict)
+        loss_summary_op_train = tf.scalar_summary("reconstruction error", loss)
 
-              if step % 100 == 0:
+
+        train_step = tf.train.MomentumOptimizer(learning_rate = 0.01, momentum = 0.9).minimize(loss)
+        #train_step = tf.train.AdamOptimizer(learning_rate = 0.001, beta1 = 0.9, beta2 = 0.999).minimize(loss)
+        sess.run(tf.initialize_all_variables())
+
+        print("\n\n")
+        print("| Training Step | Cross Entropy Train | Cross Entropy Test | Layer  |   Epoch  |")
+        print("|---------------|------------------------|-----------------------|--------|----------|")
+
+        #for step in xrange(FLAGS.pretraining_epochs * num_train):
+        for step in xrange(11000):
+            input_data, input_label = mnist.train.next_batch(FLAGS.batch_size)
+            feed_dict = {input_: np.array(input_data)}
+
+            loss_summary, loss_value = sess.run([train_step, loss],
+                                              feed_dict=feed_dict)
+
+            if step % 10 == 0:
                 summary_str = sess.run(summary_op, feed_dict=feed_dict)
                 summary_writer.add_summary(summary_str, step)
-                image_summary_op = \
-                    tf.image_summary("training_images",
-                                     tf.reshape(input_,
-                                                (FLAGS.batch_size,
-                                                 FLAGS.image_size,
-                                                 FLAGS.image_size, 1)),
-                                     max_images=FLAGS.batch_size)
 
-                summary_img_str = sess.run(image_summary_op,
-                                           feed_dict=feed_dict)
-                summary_writer.add_summary(summary_img_str)
+                total_test_loss = 0
+                for test_step in xrange(mnist.test.num_examples // FLAGS.batch_size):
+                    input_data, input_label = mnist.test.next_batch(FLAGS.batch_size)
+                    feed_dict = {input_: np.array(input_data)}
+                    test_loss = sess.run(loss, feed_dict = feed_dict)
+                    total_test_loss += test_loss * FLAGS.batch_size
 
-                output = "| {0:>13} | {1:13.4f} | Layer {2} | Epoch {3}  |"\
-                         .format(step, loss_value, n, step // num_train + 1)
+                loss_summary_op_test = tf.scalar_summary("reconstruction_error_test", total_test_loss)
+                
+                loss_summary_op = tf.merge_summary([loss_summary_op_train, loss_summary_op_test])
+                summary_scalar_str = sess.run(loss_summary_op, feed_dict = feed_dict)
+                summary_writer.add_summary(summary_scalar_str, step)
+                    
+                output = "| {0:>13} | {1:19.4f} | {2:19.4f} |Layer {3} | Epoch {4}  |"\
+                     .format(step, loss_value, total_test_loss / mnist.test.num_examples, n, step * FLAGS.batch_size//num_train + 1)
 
                 print(output)
+            if step % 1100 == 0:
+                image_summary_op = \
+                    tf.image_summary("training_images",
+                                 tf.reshape(input_,
+                                            (FLAGS.batch_size,
+                                             FLAGS.image_size,
+                                             FLAGS.image_size, 1)),
+                                 max_images=FLAGS.batch_size)
+                reconstruction_summary_op = \
+                    tf.image_summary("reconstruction_image",
+                                tf.reshape(target_for_loss, 
+                                            (FLAGS.batch_size,
+                                             FLAGS.image_size,
+                                             FLAGS.image_size, 1)),
+                                 max_images=FLAGS.batch_size)
 
-      return ae
+                summary_img_str = sess.run(image_summary_op,
+                                       feed_dict=feed_dict)
+                summary_writer.add_summary(summary_img_str, step)
+
+                summary_recon_image_str = sess.run(reconstruction_summary_op,
+                                        feed_dict = feed_dict)
+                summary_writer.add_summary(summary_recon_image_str, step)
+
+  return ae
 
 
 
