fd4d41bf5b0853fb00cff5a9a84f0b31665cd7df
jiajunshen <shenjiajun90@gmail.com>
2017-02-01 22:22:52 -0600

    update spn

--- spn/spn.py
+++ spn/spn.py
@@ -1,19 +1,21 @@
-%matplotlib inline
 import os
 os.environ['THEANO_FLAGS']='device=gpu2'
 
-import matplotlib
 import numpy as np
 np.random.seed(123)
-import matplotlib.pyplot as plt
 import lasagne
 import theano
+import sys
+import time
 import theano.tensor as T
-conv = lasagne.layers.Conv2DLayer
-pool = lasagne.layers.MaxPool2DLayer
+from lasagne.layers.dnn import Conv2DDNNLayer as conv
+from lasagne.layers.dnn import MaxPool2DDNNLayer as pool
+from dataPreparation import load_data
+
+DIM = 60
 
 def build_model(input_var=None):
-    l_in = lasagne.layers.InputLayer(shape=(None, 1, 40, 40),
+    l_in = lasagne.layers.InputLayer(shape=(None, 1, DIM, DIM),
                                      input_var = input_var)
 
     # Localization network
@@ -21,19 +23,20 @@ def build_model(input_var=None):
     b[0, 0] = 1
     b[1, 1] = 1
     b = b.flatten()
-    loc_l1 = pool(l_in, pool_size=(2, 2))
+    #loc_l1 = pool(l_in, pool_size=(2, 2))
     loc_l2 = conv(
-        loc_l1, num_filters=20, filter_size=(5, 5), W=ini)
+        l_in, num_filters=20, filter_size=(5, 5), W=lasagne.init.HeUniform())
     loc_l3 = pool(loc_l2, pool_size=(2, 2))
-    loc_l4 = conv(loc_l3, num_filters=20, filter_size=(5, 5), W=ini)
-    loc_l5 = lasagne.layers.DenseLayer(
-        loc_l4, num_units=50, W=lasagne.init.HeUniform('relu'))
+    loc_l4 = conv(loc_l3, num_filters=20, filter_size=(5, 5), W=lasagne.init.HeUniform())
+    loc_l5 = pool(loc_l4, pool_size=(2, 2))
+    loc_l6 = lasagne.layers.DenseLayer(
+        loc_l5, num_units=50, W=lasagne.init.HeUniform('relu'))
     loc_out = lasagne.layers.DenseLayer(
-        loc_l5, num_units=6, b=b, W=lasagne.init.Constant(0.0), 
+        loc_l6, num_units=6, b=b, W=lasagne.init.Constant(0.0), 
         nonlinearity=lasagne.nonlinearities.identity)
     
     # Transformer network
-    l_trans1 = lasagne.layers.TransformerLayer(l_in, loc_out, downsample_factor=1.0)
+    l_trans1 = lasagne.layers.TransformerLayer(l_in, loc_out, downsample_factor=2.0)
     print "Transformer network output shape: ", l_trans1.output_shape
     
     # Classification network
@@ -54,18 +57,20 @@ def build_model(input_var=None):
     )
     class_l4 = pool(class_l3, pool_size=(2, 2))
     class_l5 = lasagne.layers.DenseLayer(
-        lasagne.layers.dropout(class_l4,p=.5),
+        #lasagne.layers.dropout(class_l4,p=.5),
+        class_l4,
         num_units=256,
         nonlinearity=lasagne.nonlinearities.rectify,
     )
 
     l_out = lasagne.layers.DenseLayer(
-        lasagne.layers.dropout(class_l5,p=.5),
+        #lasagne.layers.dropout(class_l5,p=.5),
+        class_l5,
         num_units=10,
         nonlinearity=lasagne.nonlinearities.softmax,
     )
 
-    return l_out, l_trans1
+    return l_out, l_trans1, loc_out
 
 def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
     assert len(inputs) == len(targets)
@@ -82,7 +87,7 @@ def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
 
 def extend_image(inputs, size = 40):
     extended_images = np.zeros((inputs.shape[0], 1, size, size), dtype = np.float32)
-    margin_size = (40 - inputs.shape[2]) / 2
+    margin_size = (size - inputs.shape[2]) / 2
     extended_images[:, :, margin_size:margin_size + inputs.shape[2], margin_size:margin_size + inputs
 .shape[3]] = inputs
     return extended_images
@@ -90,10 +95,13 @@ def extend_image(inputs, size = 40):
 def main(model='mlp', num_epochs=500):
     # Load the dataset
     print("Loading data...")
-    num_per_class = 100
-    print("Using %d per class" % num_per_class)
+    # num_per_class = 100
+    # print("Using %d per class" % num_per_class)
 
-    X_train, y_train, X_test, y_test = load_data("/X_train_rotated.npy", "/Y_train_rotated.npy", "/X_test.npy", "/Y_test.npy")
+    X_train, y_train, X_test, y_test = load_data("/mnistROT.npy", "/mnistROTLabel.npy", "/mnistROTTEST.npy", "/mnistROTLABELTEST.npy", "ROT_MNIST")
+    # X_train, y_train, X_test, y_test = load_data("/X_train_limited_100.npy", "/Y_train_limited_100.npy", "/X_test.npy", "/Y_test.npy")
+    
+    """
     X_train_final = []
     y_train_final = []
     for i in range(10):
@@ -103,18 +111,19 @@ def main(model='mlp', num_epochs=500):
         y_train_final += [i] * num_per_class
     X_train = np.vstack(X_train_final)
     y_train = np.array(y_train_final, dtype = np.int32)
+    """
 
-    X_train = extend_image(X_train, 40)
-    X_test = extend_image(X_test, 40)
+    X_train = extend_image(X_train, DIM)
+    X_test = extend_image(X_test, DIM)
     #X_train, y_train, X_test, y_test = load_data("/cluttered_train_x.npy", "/cluttered_train_y.npy", "/cluttered_test_x.npy", "/cluttered_test_y.npy", dataset = "MNIST_CLUTTER")
-    _, _, X_test_rotated, y_test_rotated = load_data("/X_train.npy", "/Y_train.npy", "/X_test_rotated.npy", "/Y_test_rotated.npy")
-    X_test_rotated = extend_image(X_test_rotated, 40)
+    _, _, X_test_rotated, y_test_rotated = load_data("/mnistROT.npy", "/mnistROTLabel.npy", "/mnistROTTEST.npy", "/mnistROTLABELTEST.npy", "ROT_MNIST")
+    X_test_rotated = extend_image(X_test_rotated, DIM)
 
     # Prepare Theano variables for inputs and targets
     input_var = T.tensor4('inputs')
     target_var = T.ivector('targets')
 
-    network = build_model(input_var)
+    network, transformed_image, six_params = build_model(input_var)
     prediction = lasagne.layers.get_output(network, deterministic = False)
     prediction_eval = lasagne.layers.get_output(network, deterministic = True)
     loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
@@ -125,24 +134,34 @@ def main(model='mlp', num_epochs=500):
     eval_loss = eval_loss.mean()
     eval_acc = T.mean(T.eq(T.argmax(prediction_eval, axis = 1), target_var),
                       dtype=theano.config.floatX)
-    
-    params = lasagne.layers.get_all_params(network, trainable = True)
-    updates = lasagne.updates.adagrad(loss, params, learning_rate = 0.01)
 
-    train_fn = theano.function([input_var, target_var], loss, updates = updates)
-    eval_fn = theano.function([input_var, target_var], [eval_loss, eval_acc])
+    sh_lr = theano.shared(lasagne.utils.floatX(0.001))
+ 
+    params = lasagne.layers.get_all_params(network, trainable = True)
+    updates = lasagne.updates.adam(loss, params, learning_rate = sh_lr)
+
+    train_fn = theano.function([input_var, target_var], [loss,transformed_image_eval], updates = updates)
+    eval_fn = theano.function([input_var, target_var], [eval_loss, eval_acc, transformed_image_eval, six_params_eval])
 
     print("Starting training...")
 
     for epoch in range(num_epochs):
+        if (epoch + 1) % 20 == 0:
+            new_lr = sh_lr.get_value() * 0.7
+            sh_lr.set_value(lasagne.utils.floatX(new_lr))
         # In each epoch, we do a full pass over the training data:
         train_err = 0
         train_batches = 0
         start_time = time.time()
         for batch in iterate_minibatches(X_train, y_train, 100, shuffle=True):
             inputs, targets = batch
-            train_err += train_fn(inputs, targets)
+            train_err, trans_img = train_fn(inputs, targets)
+            train_err += train_err
             train_batches += 1
+            if train_batches == 1:
+                np.save("./unrotated_image_train.npy", trans_img)
+                np.save("./original_image_train.npy", inputs)
+        print(train_batches)
 
         # Then we print the results for this epoch:
         print("Epoch {} of {} took {:.3f}s".format(
@@ -158,7 +177,7 @@ def main(model='mlp', num_epochs=500):
             test_batches = 0
             for batch in iterate_minibatches(X_test, y_test, 500, shuffle=False):
                 inputs, targets = batch
-                err, acc = val_fn(inputs, targets)
+                err, acc, _, _ = eval_fn(inputs, targets)
                 test_err += err
                 test_acc += acc
                 test_batches += 1
@@ -173,10 +192,15 @@ def main(model='mlp', num_epochs=500):
             rotated_test_batches = 0
             for batch in iterate_minibatches(X_test_rotated, y_test_rotated, 500, shuffle=False):
                 inputs, targets = batch
-                err, acc = val_fn(inputs, targets)
+                err, acc, trans_img, six_value = eval_fn(inputs, targets)
                 rotated_test_err += err
                 rotated_test_acc += acc
                 rotated_test_batches += 1
+                if rotated_test_batches == 1:
+                    print(np.sum(six_value == 0))
+                    print(six_value)
+                    np.save("./unrotated_image.npy", trans_img)
+                    np.save("./original_image.npy", inputs)
             print("Final results:")
             print("  rotated test loss:\t\t\t{:.6f}".format(rotated_test_err / rotated_test_batches))
             print("  rotated test accuracy:\t\t{:.2f} %".format(
