a410357998764be0d346f87c6d43bf8408749699
Jiajun <shenjiajun90@gmail.com>
2017-03-15 12:43:58 -0400

    update

--- spatial_transformer_network_experiment/spn.py
+++ spatial_transformer_network_experiment/spn.py
@@ -111,7 +111,7 @@ def main():
     """
     original_data = data['X_test'][:20].reshape(20, DIM, DIM)
     transformed_data = test_transform[:20].reshape(20, DIM//3, DIM//3)
-    np.save("transform.npy", [original_data, transformed_data])
+    np.save("transform.npy", tuple(original_data, transformed_data))
 
 
 if __name__ == "__main__":
