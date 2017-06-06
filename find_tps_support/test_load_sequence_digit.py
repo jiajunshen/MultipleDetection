6dc9589f36937211e85c259935af52622ec096a1
jiajunshen <shenjiajun90@gmail.com>
2017-06-06 12:47:11 -0500

    scale support

--- find_tps_support/test_load_sequence_digit.py
+++ find_tps_support/test_load_sequence_digit.py
@@ -1,5 +1,9 @@
 from dataPreparation import load_data_digit_clutter
 import numpy as np
+import os
 
 a = load_data_digit_clutter("/X_train_limited_100.npy", "/Y_train_limited_100.npy", "/X_test.npy", "/Y_test.npy")
-np.save("saveresult.npy", a)
+
+MNIST_PATH = os.environ['MNIST']
+np.save(MNIST_PATH + "/X_test_sequence.npy", a[2])
+np.save(MNIST_PATH + "/Y_test_sequence.npy", a[3])
