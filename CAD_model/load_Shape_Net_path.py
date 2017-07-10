19538bbc9a1fbb3527e767a225cb7ecb0d9036c3
Jiajun Shen <jiajun@deeplearner.(none)>
2017-07-10 10:26:27 -0500

    shapenet path

--- CAD_model/load_Shape_Net_path.py
+++ CAD_model/load_Shape_Net_path.py
@@ -1,9 +1,14 @@
 import sys
 import os
 from create_image_pairs import create_image_pairs
+from os import listdir
+from os.path import isfile, join, isdir
+database = "SHAPENET"
+database_path = join(os.environ[database], "02691156")
+folder_list = [join(database_path, directory) for directory in listdir(database_path) if isdir(join(database_path, directory))]
 
-database = "IKEA"
-database_path = os.environ[database]
+print(folder_list)
+"""
 obj_file = open(database_path + "/all_obj.txt", "r")
 content = obj_file.readlines()
 content = [x.strip() for x in content]
@@ -26,3 +31,4 @@ for item_type, item_obj in zip(furniture_type, furniture_obj):
     save_path_list.append(item_type + "_" + item_obj)
 
 create_image_pairs(obj_path_list, save_path_list)
+"""
