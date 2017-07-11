8c7274568af60e63e49a6163e1a1659978f0f507
Jiajun <shenjiajun90@gmail.com>
2017-07-11 14:23:10 -0400

    convert numpy

--- CAD_model/convert_numpy_data.py
+++ CAD_model/convert_numpy_data.py
@@ -13,6 +13,7 @@ texture_files = [join(texture_file_path, f) for f in listdir(texture_file_path) 
 
 from scipy import misc
 plain_images = []
+plain_images_tmp = []
 for f in plain_files:
     image = misc.imread(f)
     mask = np.array(np.product(image, axis = 2) == 237 * 237 * 255 * 255).reshape(500,500,1)
@@ -25,10 +26,16 @@ for f in plain_files:
     new_image[:,:,1] = (1 - image[:,:,3]) * image[:,:,1] + image[:,:,3] * image[:,:,1]
     new_image[:,:,2] = (1 - image[:,:,3]) * image[:,:,2] + image[:,:,3] * image[:,:,2]
     new_image = bgcolor * mask_all + new_image * (1 - mask_all)
-    plain_images.append(new_image)
-plain_images = np.asarray(plain_images)
+    if len(plain_images_tmp) < 100:
+        plain_images_tmp.append(new_image)
+    else:
+        plain_images.append(np.asarray(plain_images_tmp, dtype=np.float32))
+        plain_images_tmp = []
+
+plain_images = np.vstack(plain_images)
 
 texture_images = []
+texture_images_tmp = []
 for f in texture_files:
     image = misc.imread(f)
     mask = np.array(np.product(image, axis = 2) == 237 * 237 * 255 * 255).reshape(500,500,1)
@@ -41,10 +48,15 @@ for f in texture_files:
     new_image[:,:,1] = (1 - image[:,:,3]) * image[:,:,1] + image[:,:,3] * image[:,:,1]
     new_image[:,:,2] = (1 - image[:,:,3]) * image[:,:,2] + image[:,:,3] * image[:,:,2]
     new_image = bgcolor * mask_all + new_image * (1 - mask_all)
-    texture_images.append(new_image)
-texture_images = np.asarray(texture_images)
+    if len(texture_images_tmp) < 100:
+        texture_images_tmp.append(new_image)
+    else:
+        texture_images.append(np.asarray(texture_images_tmp, dtype=np.float32))
+        texture_images_tmp = []
 
-how_image = np.zeros((1000, 5000, 3), dtype =np.float32)
+texture_images = np.vstack(texture_images)
+
+show_image = np.zeros((1000, 5000, 3), dtype =np.float32)
 for i in range(10):
     show_image[:500, i * 500:i * 500 + 500] = plain_images[i][:,:,:]
     show_image[500:1000, i * 500:i * 500 + 500] = texture_images[i][:,:,:]
