b6602f2dfcb3a3e18ca8c38f0523baee81171fa9
Jiajun <shenjiajun90@gmail.com>
2017-07-12 16:22:16 -0400

    fix

--- CAD_model/load_Shape_Net_path.py
+++ CAD_model/load_Shape_Net_path.py
@@ -8,7 +8,7 @@ database = "SHAPENET"
 folder_path_list = []
 folder_name_list = []
 all_object_class = []
-object_list = ["02808440", "03207941", "03642806", "03691469", "03761084", "04515003"]
+object_list = ["02808440", "03207941", "03642806", "03691459", "03761084", "04515003"]
 for object_name in object_list:
     database_path = join(os.environ[database], object_name)
     folder_path_list += [join(database_path, directory) for directory in listdir(database_path) if isdir(join(database_path, directory))]
